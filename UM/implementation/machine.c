70ad0e13e8e60826dc8bb872abca23fa8167a6c8
Jay Facultad <jayfacultad@gmail.com>
2018-11-14 20:22:45 -0500

    Corrected issue with .um files

--- UM/implementation/machine.c
+++ UM/implementation/machine.c
@@ -13,11 +13,20 @@
 
 const int byte_len = 8;
 
+// void Bitpack_printu(uint32_t word)
+// {
+//         uint32_t mask = Bitpack_mask(1, TYPE_SIZE - 1);
+//         for(int i - TYPE_SIZE; i >o; i--) {
+
+
+//         }
+// }
+
 
 int main (int argc, char *argv[])
 {
         FILE * fp;
-        uint32_t $r[8];                      // Registers
+        uint32_t $r[8];                 // Registers
         Seg_T $m = Seg_new(1000);       // Segments
         Seg_T stack = Seg_new(1);       // Stack of unmapped segments
 
@@ -33,30 +42,35 @@ int main (int argc, char *argv[])
         struct stat st;
         stat(argv[1], &st);
         int file_size = st.st_size;
-        int num_words = file_size / sizeof(uint32_t);
-        // fprintf(stderr, "%d\n", num_words);
+        int num_words = file_size / (sizeof(uint32_t)); // File size impacted by spaces
+        fprintf(stderr, "File size: %d\n", file_size);
+        fprintf(stderr, "Number of words: %d\n", num_words);
         // int file_size = fstat(fd, st);
 
         // Create and load Segment 0 with all of the instructions
         uint32_t arr[num_words];
         Seg_map($m, stack, arr);        
         int offset = 0;
-        while (feof(fp) == 0) {
+        for (int j = 0; j < num_words; j++) {
                 uint32_t word = 0;
-                for (int i = 0; i < 4; i++) {
-                        uint8_t extract_byte = (uint8_t)getc(fp);
-                        word = Bitpack_news(word, byte_len, (24 - (byte_len  * i)), extract_byte);
-                        fprintf(stderr, "%c\n", extract_byte);
-                        fprintf(stderr, "%c\n", word);
+                for (int i = 0; i < 4; i++) { // To disregard the space character
+                        char extract_byte = (uint8_t)getc(fp);
+                        if (i < 4) {
+                                word = Bitpack_news(word, byte_len, (24 - (byte_len  * i)), extract_byte);
+                                fprintf(stderr, "%x\n", extract_byte);    
+                        }
+                        // NOTE: spaces are printed as hex 20
+                        
                 }
+                printf("Word: %x\n", word);
                 arr[offset++] = word;
         }
         Seg_put($m, 0, arr);
 
         /* Testing extraction of value in segments structure */
         uint32_t (*temp)[num_words] = Seg_get($m, 0);
-        printf("%" PRIu32"\n", *temp[0]);
-        printf("\n");
+        // printf("%c\n", *temp[0]);
+        // printf("\n");
         (void)temp;
         (void)$r;
         (void)fd;
