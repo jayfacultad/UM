278a8678638061a329ec1ce7442fb716a1d437d7
Jay Facultad <jayfacultad@gmail.com>
2018-11-15 18:10:34 -0500

    Added function calls to switch statement

--- UM/implementation/machine.c
+++ UM/implementation/machine.c
@@ -30,12 +30,12 @@ void multiplication(uint32_t (*$r)[], short a, short b, short c);
 void division(uint32_t (*$r)[], short a, short b, short c);
 void bitwise_nand(uint32_t (*$r)[], short a, short b, short c);
 void halt(uint32_t (*$r)[], short a, short b, short c);
-void map_segment(uint32_t (*$r)[], Seg_T *$m, int num_words, short a, short b, short c);
-void umap_segment(uint32_t (*$r)[], Seg_T *$m, int num_words, short a, short b, short c);
+void map_segment(uint32_t (*$r)[], Seg_T *$m, Seg_T *stack, int num_words, short a, short b, short c);
+void umap_segment(uint32_t (*$r)[], Seg_T *$m, Seg_T *stack, int num_words, short a, short b, short c);
 void output(uint32_t (*$r)[], short a, short b, short c);
 void input(uint32_t (*$r)[], short a, short b, short c);
 void load_program(uint32_t (*$r)[], Seg_T *$m, int num_words, short a, short b, short c);
-void load_value((*$r)[], short a);
+void load_value(uint32_t (*$r)[], short a);
 
 
 int main (int argc, char *argv[])
@@ -61,15 +61,14 @@ int main (int argc, char *argv[])
         int num_words = file_size / (sizeof(uint32_t)); 
         fprintf(stderr, "File size: %d\n", file_size);
         fprintf(stderr, "Number of words: %d\n", num_words);
-        // int file_size = fstat(fd, st);
 
-        // Create and load Segment 0 with all of the instructions
+        // Create and load Segment 0 with every instruction
         uint32_t arr[num_words];
         Seg_map($m, stack, arr);        
         int offset = 0;
-        for (int j = 0; j < num_words; j++) {
+        for (int i = 0; i < num_words; i++) {
                 uint32_t word = 0;
-                for (int i = 0; i < 4; i++) { 
+                for (int j = 0; j < 4; j++) { 
                         char extract_byte = (uint8_t)getc(fp);
                         word = Bitpack_news(word, byte_len, (24 - (byte_len  * i)), extract_byte);             
                 }
@@ -102,29 +101,29 @@ int main (int argc, char *argv[])
                                 break;
                         case 2: segmented_store(&$r, &$m, num_words, a, b, c);
                                 break;
-                        case 3: // do something
+                        case 3: addition(&$r, a, b, c);
                                 break;
-                        case 4: // do something
+                        case 4: multiplication(&$r, a, b, c);
                                 break;   
-                        case 5: // do something
+                        case 5: division(&$r, a, b, c);
                                 break;
-                        case 6: // do something
+                        case 6: bitwise_nand(&$r, a, b, c);
                                 break;     
-                        case 7: // do something
+                        case 7: halt(&$r, a, b, c);
                                 break;     
-                        case 8: // do something
+                        case 8: map_segment(&$r, &$m, &stack, num_words, a, b, c);
                                 break;     
-                        case 9: // do something
+                        case 9: umap_segment(&$r, &$m, &stack, num_words, a, b, c);
                                 break;     
-                        case 10: // do something
+                        case 10: output(&$r, a, b, c);
                                 break;     
-                        case 11: // do something
+                        case 11: input(&$r, a, b, c);
                                 break;     
-                        case 12: // do something
+                        case 12: load_program(&$r, &$m, num_words, a, b, c);
                                 break;     
-                        case 13: // do something
+                        case 13: load_value(&$r, a);
                                 break;              
-                        default: // do something
+                        default: fprintf(stderr, "Error: Unknown Instruction.\n");
                                 return EXIT_FAILURE;
                 }
 
@@ -159,57 +158,96 @@ void segmented_store(uint32_t (*$r)[], Seg_T *$m, int num_words, short a, short 
 
 void addition(uint32_t (*$r)[], short a, short b, short c)
 {
-
+        (void)$r;
+        (void)a;
+        (void)b;
+        (void)c;
 }
 
 void multiplication(uint32_t (*$r)[], short a, short b, short c)
 {
-
+        (void)$r;
+        (void)a;
+        (void)b;
+        (void)c;
 }
 
 void division(uint32_t (*$r)[], short a, short b, short c)
 {
-
+        (void)$r;
+        (void)a;
+        (void)b;
+        (void)c;
 }
 
 void bitwise_nand(uint32_t (*$r)[], short a, short b, short c)
 {
-
+        (void)$r;
+        (void)a;
+        (void)b;
+        (void)c;
 }
 
 void halt(uint32_t (*$r)[], short a, short b, short c)
 {
-
+        (void)$r;
+        (void)a;
+        (void)b;
+        (void)c;
 }
 
-void map_segment(uint32_t (*$r)[], Seg_T *$m, int num_words, short a, short b, short c) 
+void map_segment(uint32_t (*$r)[], Seg_T *$m, Seg_T *stack, int num_words, short a, short b, short c) 
 {
-
+        (void)$r;
+        (void)$m;
+        (void)stack;
+        (void)num_words;
+        (void)a;
+        (void)b;
+        (void)c;
 }
 
-void umap_segment(uint32_t (*$r)[], Seg_T *$m, int num_words, short a, short b, short c)
+void umap_segment(uint32_t (*$r)[], Seg_T *$m, Seg_T *stack, int num_words, short a, short b, short c)
 {
-
+        (void)$r;
+        (void)$m;
+        (void)stack;
+        (void)num_words;
+        (void)a;
+        (void)b;
+        (void)c;
 }
 
 void output(uint32_t (*$r)[], short a, short b, short c)
 {
-
+        (void)$r;
+        (void)a;
+        (void)b;
+        (void)c;
 }
 
 void input(uint32_t (*$r)[], short a, short b, short c)
 {
-
+        (void)$r;
+        (void)a;
+        (void)b;
+        (void)c;
 }
 
 void load_program(uint32_t (*$r)[], Seg_T *$m, int num_words, short a, short b, short c)
 {
-
+        (void)$r;
+        (void)$m;
+        (void)num_words;
+        (void)a;
+        (void)b;
+        (void)c;
 }
 
-void load_value((*$r)[], short a)
+void load_value(uint32_t (*$r)[], short a)
 {
-        
+        (void)$r;
+        (void)a;
 }
 
 
